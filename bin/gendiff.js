c2e1954196d1dd1723f7f0cd3902b02033d7876b
Alekcandr Eremin <eremin.alek2014@yandex.ru>
2024-10-09 19:40:27 +0300

    'fix'

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,12 +8,13 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .argument('<filepath1>')
   .argument('<filepath2>')
   .action((filepath1, filepath2, options) => {
-    const result = genDiff(filepath1, filepath2, options.format);
-    console.log(result);
+    const format = options.format || 'stylish';
+    console.log(genDiff(filepath1, filepath2, format));
+    return genDiff(filepath1, filepath2, format);
   });
 
 program.parse();
