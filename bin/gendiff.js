b0fc47f3509b7f6ac708962b7df7d359ed3867c4
Alekcandr Eremin <eremin.alek2014@yandex.ru>
2025-04-03 11:25:07 +0300

    'fix'

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,20 +1,7 @@
 #!/usr/bin/env node
-import { Command } from 'commander';
-import genDiff from '../src/gendifflogic.js';
-
+import { Command } from 'commander/esm.mjs';
 const program = new Command();
-
 program
-  .name('gendiff')
-  .description('Compares two configuration files and shows a difference.')
-  .arguments('<filepath1> <filepath2>')
-  .version('1.0.0', '-V, --version', 'output the version number')
-  .helpOption('-h, --help', 'output usage information')
-  .option('-f, --format [type]', 'output format', 'stylish')
-  .action((filepath1, filepath2, options) => {
-    const format = options.format || 'stylish';
-    console.log(genDiff(filepath1, filepath2, format));
-    return genDiff(filepath1, filepath2, format);
-  });
-
-program.parse(process.argv);
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .description('Compares two configuration files and shows a difference.');
+program.parse();
